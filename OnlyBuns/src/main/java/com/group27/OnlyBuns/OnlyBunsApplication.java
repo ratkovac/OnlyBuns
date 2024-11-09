88181f8f216eaa14221d6a6be64641d0af019c61
Nikola Ratkovac <nikolaratkovac3@gmail.com>
2024-11-09 22:20:34 +0100

    prvi commit

---
+++ OnlyBuns/src/main/java/com/group27/OnlyBuns/OnlyBunsApplication.java
@@ -0,0 +1,13 @@
+package com.group27.OnlyBuns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyBunsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyBunsApplication.class, args);
+	}
+
+}
