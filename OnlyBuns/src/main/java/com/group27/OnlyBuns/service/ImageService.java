40cea5777809a797cf85d65ff71d00e502a5907f
ra186-2021 <janicevicivan90@gmail.com>
2024-11-11 15:17:09 +0100

    Implemented image compression with scheduling

---
+++ OnlyBuns/src/main/java/com/group27/OnlyBuns/service/ImageService.java
@@ -0,0 +1,52 @@
+package com.group27.OnlyBuns.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalTime;
+import java.util.Date;
+import net.coobird.thumbnailator.Thumbnails;
+
+@Service
+public class ImageService {
+
+    @Value("${image.upload-dir}")
+    private String imageDir;
+
+    @Value("${image.compressed-dir}")
+    private String compressedDir;
+
+    @Scheduled(cron = "0 13 15 * * ?")
+    public void compressOldImages() {
+        File dir = new File(imageDir);
+        File[] files = dir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                long diffInMillies = new Date().getTime() - file.lastModified();
+                long diffInDays = diffInMillies / (1000 * 60 * 60 * 24);
+
+                if (diffInDays < 30) {
+                    try {
+                        File compressedDirectory = new File(compressedDir);
+                        if (!compressedDirectory.exists()) {
+                            compressedDirectory.mkdirs();
+                        }
+
+                        File compressedFile = new File(compressedDirectory, file.getName());
+
+                        Thumbnails.of(file)
+                                .size(800, 800)
+                                .outputFormat("jpg")
+                                .outputQuality(0.75)
+                                .toFile(compressedFile);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
