5db5d22815120b18105422c2534b00de2a4410b2
ra186-2021 <janicevicivan90@gmail.com>
2024-11-12 22:25:02 +0100

    fix: images compression

--- OnlyBuns/src/main/java/com/group27/OnlyBuns/OnlyBunsApplication.java
+++ OnlyBuns/src/main/java/com/group27/OnlyBuns/OnlyBunsApplication.java
@@ -8,10 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.Clock;
 
 @SpringBootApplication
+@EnableScheduling
 public class OnlyBunsApplication {
 
 	public static void main(String[] args) {
